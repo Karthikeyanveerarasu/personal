b2675d26b7aefac271f3684f8a0ac679ba4894fc
KARTHIKEYAN V V <111226561+Karthikeyanveerarasu@users.noreply.github.com>
2023-01-15 19:28:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import Main from './components/Main'
+import Skill from './components/Skill'
+import Project from './components/Project'
+import Wok from './components/Wok'
+export default function App() {
+  return (
+    <div className="App">
+      <Main />
+      <Skill />
+      <Project />
+      
+      </div>
+  );
+}
