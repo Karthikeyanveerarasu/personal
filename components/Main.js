c6213f9f66d6fec2209c4417c4cc906fcb5d0670
KARTHIKEYAN V V <111226561+Karthikeyanveerarasu@users.noreply.github.com>
2023-01-15 19:53:29 +0530

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -2,17 +2,18 @@
 import './MainStyle.css';
 import videoBg from '../assets/videoBg.mp4'
 import Typewriter from 'typewriter-effect'
-import FadeIn from 'react-fade-in';
+import Fade from 'react-reveal/Fade'
+import RubberBand from 'react-reveal/RubberBand'
 export default function Main(){
   return(
     <>
-    <FadeIn transitionDuration={2000} delay={100}>
     <div className="main">
       <div className="overlay"></div>
       <video src={videoBg} autoPlay muted loop/>
       <div className="content">
+        <RubberBand count={3}>
         <h2>Hi  <span style={{color:"orange"}}> I'm</span>
-      </h2>
+      </h2></RubberBand>
         <span className="my-3" style={{color:"white",fontSize:"1.8rem",marginBottom:"20px",fontWeight:"bold",letterSpacing:"2px"}}>
         <Typewriter
        options={{
@@ -39,7 +40,7 @@ export default function Main(){
         </div>
       </div>
       </div>
-      </FadeIn>
+      
     </>
   )
 }
