6676ed90375f9600ceb802ed7466272bd9cb0a21
KARTHIKEYAN V V <111226561+Karthikeyanveerarasu@users.noreply.github.com>
2023-01-17 12:17:43 +0530

    Update Like.js

--- components/Like.js
+++ components/Like.js
@@ -1,19 +1,26 @@
-import react from 'react'; 
 import Bounce from 'react-reveal/Bounce'
 import { Provider } from '@lyket/react'
 import { LikeButton } from '@lyket/react'
-import './Like.css';
+
 export default function Like(){
   return(
     <div className="text-center rounded " style={{backgroundColor:"",width:"80%",margin:"auto"}}>
-    <Bounce count={3}>  <h2 className="pt-5 text-black" style={{letterSpacing:"1px"}}>Give Me a <span style={{color:"red"}}>Thumbs</span> Up ! </h2></Bounce>
-      <Provider apiKey="acc0dbccce8e557db5ebbe6d605aaa">
+    <Bounce count={3}>  <h2 className="pt-5 text-black" style={{letterSpacing:"1px"}}>Do You <span style={{color:"red"}}>Like</span> This Design !</h2></Bounce>
+      <Provider apiKey="acc0dbccce8e557db5ebbe6d605aaa"
+      theme={
+        {
+          colors:{
+                  text:"red",
+                         }
+        }
+      }
+       >
       <LikeButton 
       namespace="hello"
       id="like"
-      className="likebtn"
-      />
-      
+      component={LikeButton.templates.Twitter}
+     hideCounterIfLessThan={0}
+     />     
         </Provider>
     </div>
   )
